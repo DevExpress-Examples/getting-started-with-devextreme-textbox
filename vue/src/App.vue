78252598c319aa59d83343b0cd807dbc4cf752fc
skorokhodova.vlada <vlada.skorokhodova@devexpress.com>
2022-03-21 10:49:49 +0300

    Add an example

---
+++ vue/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="container">
+        <DxTextBox
+            v-model:value="value"
+            mode="url"
+            label="Link"
+            label-mode="floating"
+            :max-length="20"
+            :show-clear-button="true"
+            @enterKey="onEnterKey($event)"
+        /> 
+    </div>
+</template>
+
+<script>
+    import { DxTextBox } from 'devextreme-vue/text-box';
+
+    export default {
+        components: {
+            DxTextBox
+        },
+        data() {
+            return {
+                value: ""
+            }
+        },
+        methods: {
+            onEnterKey() {
+                console.log(this.value);
+            }
+        }
+    }
+</script>
+
+<style>
+    #container {
+        width: 250px;
+    }
+</style>
