30c1deaec59b9846b10ed23f9fb5010703af2db9
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-22 11:59:11 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -3,9 +3,10 @@ import notify from 'devextreme/ui/notify';
 import { DxTextBoxTypes } from 'devextreme-angular/ui/text-box';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   textBoxValue = '';
